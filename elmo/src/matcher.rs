6af38400cb3478f7d2868cd41ea31c751990fb0f
Eric Sink <eric@zumero.com>
2015-09-17 12:31:13 -0500

    expand the matcher walk_path code to LT and LTE

--- elmo/src/matcher.rs
+++ elmo/src/matcher.rs
@@ -577,6 +577,35 @@ fn match_pair<F: Fn(usize)>(pred: &Pred, path: &str, start: &bson::Value, cb_arr
     let walk = start.walk_path(path);
 
     match pred {
+        &Pred::LT(ref lit) => {
+
+            let new = 
+                walk.leaves().any(
+                    |ov| {
+                        match ov {
+                            Some(v) => {
+                                cmp_with_array(cmp_lt, v, lit)
+                            },
+                            None => {
+                                false
+                            },
+                        }
+                    }
+                );
+            let old = match_pair_other(pred, path, start, false, cb_array_pos);
+            if new != old {
+                println!("BEGIN LT fail");
+                println!("    new = {:?}", new);
+                println!("    old = {:?}", old);
+                println!("    lit = {:?}", lit);
+                println!("    start = {:?}", start);
+                println!("    path = {:?}", path);
+                println!("    walk = {:?}", walk);
+                println!("    leaves = {:?}", walk.leaves().collect::<Vec<_>>());
+                println!("    END LT fail");
+            }
+            old
+        },
         &Pred::EQ(ref lit) => {
 
             let new = 
@@ -610,6 +639,42 @@ fn match_pair<F: Fn(usize)>(pred: &Pred, path: &str, start: &bson::Value, cb_arr
                 println!("    walk = {:?}", walk);
                 println!("    leaves = {:?}", walk.leaves().collect::<Vec<_>>());
                 println!("    END EQ fail");
+            }
+            old
+        },
+        &Pred::LTE(ref lit) => {
+
+            let new = 
+                walk.leaves().any(
+                    |ov| {
+                        match ov {
+                            Some(v) => {
+                                cmp_with_array(cmp_lte, v, lit)
+                            },
+                            None => {
+                                // If we have a path leaf (as we have defined it) that
+                                // results in missing value, and if we are being asked to
+                                // compare to null, then we consider that a match.
+                                match lit {
+                                    // TODO not sure if BUndefined needs to be included here
+                                    &bson::Value::BNull | &bson::Value::BUndefined => true,
+                                    _ => false,
+                                }
+                            },
+                        }
+                    }
+                );
+            let old = match_pair_other(pred, path, start, false, cb_array_pos);
+            if new != old {
+                println!("BEGIN LTE fail");
+                println!("    new = {:?}", new);
+                println!("    old = {:?}", old);
+                println!("    lit = {:?}", lit);
+                println!("    start = {:?}", start);
+                println!("    path = {:?}", path);
+                println!("    walk = {:?}", walk);
+                println!("    leaves = {:?}", walk.leaves().collect::<Vec<_>>());
+                println!("    END LTE fail");
             }
             old
         },
