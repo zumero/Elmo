594aba2d9aa6ef29b024dd7610134ac70f42f61b
Eric Sink <eric@zumero.com>
2015-10-05 15:30:46 -0500

    add a merge command to diag_lsm

--- diag_lsm/src/main.rs
+++ diag_lsm/src/main.rs
@@ -14,10 +14,11 @@
     limitations under the License.
 */
 
+#![feature(convert)]
+
 extern crate lsm;
 
-fn work(name: &str) -> Result<(),lsm::Error> {
-    println!("file: {}", name);
+fn list_segments(name: &str) -> Result<(),lsm::Error> {
     let db = try!(lsm::db::new(String::from(name), lsm::DEFAULT_SETTINGS));
     let (segments, infos) = try!(db.list_segments());
     println!("segments: {:?}", segments);
@@ -25,8 +26,60 @@ fn work(name: &str) -> Result<(),lsm::Error> {
     Ok(())
 }
 
-pub fn main() {
+fn merge(name: &str, level: u32, min: usize, max: Option<usize>) -> Result<(),lsm::Error> {
+    let db = try!(lsm::db::new(String::from(name), lsm::DEFAULT_SETTINGS));
+    match try!(db.merge(level, min, max)) {
+        Some(seg) => {
+            println!("merged segment: {}", seg);
+            let lck = try!(db.GetWriteLock());
+            try!(lck.commitMerge(seg));
+            Ok(())
+        },
+        None => {
+            println!("no merge needed");
+            Ok(())
+        },
+    }
+}
+
+fn result_main() -> Result<(),lsm::Error> {
     let args: Vec<_> = std::env::args().collect();
-    work(&args[1]);
+    println!("args: {:?}", args);
+    if args.len() < 2 {
+        return Err(lsm::Error::Misc("no command given"));
+    }
+    let cmd = args[1].as_str();
+    if args.len() < 3 {
+        return Err(lsm::Error::Misc("no filename given"));
+    }
+    let name = args[2].as_str();
+    match cmd {
+        "merge" => {
+            if args.len() < 5 {
+                return Err(lsm::Error::Misc("too few args"));
+            }
+            let level = args[3].parse::<u32>().unwrap();
+            let min = args[4].parse::<usize>().unwrap();
+            if args.len() == 5 {
+                merge(name, level, min, None)
+            } else {
+                let max = args[5].parse::<usize>().unwrap();
+                merge(name, level, min, Some(max))
+            }
+        },
+        "list_segments" => {
+            list_segments(name)
+        },
+        _ => {
+            Err(lsm::Error::Misc("unknown command"))
+        },
+    }
+}
+
+pub fn main() {
+    let r = result_main();
+    if r.is_err() {
+        println!("{:?}", r);
+    }
 }
 
